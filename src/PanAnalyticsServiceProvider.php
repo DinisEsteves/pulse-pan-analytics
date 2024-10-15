c4b1f2a6188491b6543062adc6615caaf3ff4267
DinisEsteves <DinisEsteves@users.noreply.github.com>
2024-10-15 21:32:56 +0000

    Fix styling

--- src/PanAnalyticsServiceProvider.php
+++ src/PanAnalyticsServiceProvider.php
@@ -4,13 +4,12 @@ namespace VendorName\Skeleton;
 
 use Spatie\LaravelPackageTools\Package;
 use Spatie\LaravelPackageTools\PackageServiceProvider;
-use VendorName\Skeleton\Commands\SkeletonCommand;
 
 class PanAnalyticsServiceProvider extends PackageServiceProvider
 {
     public function configurePackage(Package $package): void
     {
-        $this->loadViewsFrom(__DIR__ . '/../resources/views', 'outdated');
+        $this->loadViewsFrom(__DIR__.'/../resources/views', 'outdated');
 
         $this->callAfterResolving('livewire', function (LivewireManager $livewire, Application $app) {
             $livewire->component('outdated', Outdated::class);
